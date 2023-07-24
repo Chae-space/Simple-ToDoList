34062c7c82df95af5489f62bb624094bf411505d
Chae-space <lena8530@nate.com>
2023-07-24 16:46:08 +0900

    생성/제거/완료

--- src/main/java/com/my/todolist/TodoController.java
+++ src/main/java/com/my/todolist/TodoController.java
@@ -4,7 +4,10 @@ import java.util.List;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class TodoController {
@@ -25,6 +28,28 @@ public class TodoController {
 
     @RequestMapping("/")
     public String home() {
+        return "redirect:/todo";
+    }
+
+    @PostMapping("/todo/create")
+    public String create(@RequestParam("todo-desc") String content) {
+        // 새로운 TODO를 생성하는 컨트롤러 메소드
+        todoService.createToDo(content);
+        return "redirect:/todo";
+    }
+
+    @PostMapping("/todo/{id}/update")
+    public String update(@PathVariable("id") Long id) {
+        // Todo의 done 상태를 변경하는 메소드
+        todoService.updateToDo(id);
+        return "redirect:/todo";
+
+    }
+
+    @PostMapping("/todo/{id}/delete")
+    public String delete(@PathVariable("id") Long id) {
+        // TODO를 삭제하는 메소드
+        todoService.deleteToDo(id);
         return "redirect:/todo";
     }
 
